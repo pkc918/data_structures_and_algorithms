76522b1e1e69f5ae09b0aaf8690a4b1c07ed6321
潘克淳 <pankechun@weifengqi18.com>
2022-12-30 10:11:32 +0800

    测试queue

--- main/main.go
+++ main/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"data_structures_and_algorithms/queue"
+	"fmt"
+)
 
+func main() {
+	fmt.Println("===== Queue =====")
+	myQueue := &queue.Queue{
+		MaxSize: 5,
+		Front:   -1,
+		Rear:    -1,
+	}
+	fmt.Print("初始化-打印队列元素：")
+	myQueue.TraversalQueue()
+	// 入队
+	for i := 0; i < 4; i++ {
+		err := myQueue.AddQueue(i)
+		if err != nil {
+			return
+		}
+	}
+	fmt.Println()
+	fmt.Print("添加元素-打印队列元素：")
+	myQueue.TraversalQueue()
+	// 获取
+	index := 1
+	val, err := myQueue.GetEleByQueue(index)
+	if err != nil {
+		return
+	}
+	fmt.Println()
+	fmt.Printf("获取下标为%d的元素：%d", index, val)
+	// 出队
+	shiftQueue, err := myQueue.ShiftQueue()
+	if err != nil {
+		return
+	}
+	fmt.Println()
+	fmt.Printf("当前出队元素：%d", shiftQueue)
+	fmt.Println()
+	fmt.Print("出队-打印队列元素")
+	myQueue.TraversalQueue()
 }
